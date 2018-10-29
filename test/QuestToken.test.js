9e11aa8a75f083b5da07f0d7ae1810a7c67b5f38
Kevin Weiler <kevinw@gmail.com>
2018-10-29 13:57:59 -0700

    More unit testing on Quest Token contract, remove coordinates on quest

--- test/QuestToken.test.js
+++ test/QuestToken.test.js
@@ -18,11 +18,11 @@ const should = require(`chai`)
 
 const saveRasheedQuestId = `3430083573232153044767320670749830693485949750982054416035926456`
 const saveRasheedQuestIPFS = `QmVLGZhFZNACQfBZFUPgMvsXU7PiDnSBqgt7ob4AusXPuY`
-const saveRasheedQuestLord = `0x3D01dDdB4eBD0b521f0E4022DCbeF3cb9bc20FF2`
+const saveRasheedQuestIPFS1 = `QmVLGZhFZNACQfBZFUPgMvsXU7PiDnSBqgt7ob4AusXPud`
 
 contract(
   `QuestToken`,
-  ([contractOwner, questCreator, questHero, questHero2]) => {
+  ([contractOwner, questCreator, questCreator2, questHero, questHero2]) => {
     before(async () => {
       this.questLibrary = await QuestLibrary.new()
     })
@@ -51,7 +51,7 @@ contract(
         this.repeatLimit,
         saveRasheedQuestIPFS,
         questOwner,
-        { from: questOwner }
+        { from: questOwner, gasPrice: 0 }
       )
       .then(async () => increase(duration.seconds(1)))
 
@@ -65,7 +65,7 @@ contract(
       1,
       hero,
       saveRasheedQuestIPFS,
-      { from: owner }
+      { from: owner, gasPrice: 0 }
     )
 
     describe(`Quest Creation`, () => {
@@ -161,7 +161,6 @@ contract(
         this.repeatLimit = 2
         this.questSupply = 4
         await createQuest(questCreator).should.be.fulfilled
-        // console.log(`M0`, metadata0)
         const params1 = [
           saveRasheedQuestId,
           1,
@@ -177,17 +176,142 @@ contract(
           { from: questCreator }
         ]
         const q1 = await this.questToken.completeQuest.call(...params1)
-
         await this.questToken.completeQuest(...params1)
         const q2 = await this.questToken.completeQuest.call(...params2)
-
         await this.questToken.completeQuest(...params2)
 
-        // console.log(`M1`, metadata1)
         const index = await this.questLibrary.getTokenData(q1)
         index.should.be.bignumber.equal(0)
         const index2 = await this.questLibrary.getTokenData(q2)
         index2.should.be.bignumber.equal(1)
+      })
+    })
+    describe(`decentralized quests`, () => {
+      beforeEach(async () => {
+        await createQuest().should.be.fulfilled
+      })
+      it(`should accept up to one pending proof`, async () => {
+        const params = [
+          saveRasheedQuestId,
+          saveRasheedQuestIPFS,
+          { from: questHero, value: this.questCost }
+        ]
+        const params1 = [
+          saveRasheedQuestId,
+          saveRasheedQuestIPFS,
+          { from: questHero2, value: this.questCost }
+        ]
+        await this.questToken.submitProofs(...params).should.be.fulfilled
+        await this.questToken.submitProofs(...params1).should.not.be.fulfilled
+      })
+      it(`should not allow identical proofs`, async () => {
+        const params = [
+          saveRasheedQuestId,
+          saveRasheedQuestIPFS,
+          { from: questHero, value: this.questCost }
+        ]
+        const params1 = [
+          saveRasheedQuestId,
+          saveRasheedQuestIPFS,
+          { from: questHero2, value: this.questCost }
+        ]
+        await this.questToken.submitProofs(...params).should.be.fulfilled
+        await this.questToken.submitProofs(...params1).should.not.be.fulfilled
+      })
+      it(`should allow different proofs from different heros`, async () => {
+        const params = [
+          saveRasheedQuestId,
+          saveRasheedQuestIPFS,
+          { from: questHero, value: this.questCost }
+        ]
+        const params1 = [
+          saveRasheedQuestId,
+          saveRasheedQuestIPFS1,
+          { from: questHero2, value: this.questCost }
+        ]
+        await this.questToken.submitProofs(...params).should.be.fulfilled
+        await this.questToken.submitProofs(...params1).should.be.fulfilled
+        const pending = await this.questToken.pendingProofs.call(
+          saveRasheedQuestId,
+          questHero
+        )
+        const pending2 = await this.questToken.pendingProofs.call(
+          saveRasheedQuestId,
+          questHero2
+        )
+        pending[1].should.be.equal(saveRasheedQuestIPFS)
+        pending2[1].should.be.equal(saveRasheedQuestIPFS1)
+      })
+      it(`should pay quest owner for validating proofs`, async () => {
+        const balance = await web3.eth.getBalance(questCreator)
+
+        const params = [
+          saveRasheedQuestId,
+          saveRasheedQuestIPFS,
+          { from: questHero, value: this.questCost }
+        ]
+        await this.questToken.submitProofs(...params).should.be.fulfilled
+        await completeQuest(questHero).should.be.fulfilled
+        const balance1 = await web3.eth.getBalance(questCreator)
+        balance.should.be.bignumber.equal(balance1.minus(this.questCost))
+      })
+
+      it(`should allow refunds on submitted proofs after 50 blocks`, async () => {
+        const params = [
+          saveRasheedQuestId,
+          saveRasheedQuestIPFS,
+          { from: questHero, value: this.questCost }
+        ]
+        await this.questToken.submitProofs(...params).should.be.fulfilled
+        await this.questToken.requestRefund(saveRasheedQuestId, {
+          from: questHero
+        }).should.not.be.fulfilled
+        const promises = Array(52)
+          .fill()
+          .map(_ => advanceBlock())
+        await Promise.all(promises)
+        const balance = await web3.eth.getBalance(questHero)
+        await this.questToken.requestRefund(saveRasheedQuestId, {
+          from: questHero,
+          gasPrice: 0
+        }).should.be.fulfilled
+        const balance1 = await web3.eth.getBalance(questHero)
+        balance.should.be.bignumber.equal(balance1.minus(this.questCost))
+      })
+
+      it(`can approve reclaiming by only quest owner`, async () => {
+        const params = [
+          saveRasheedQuestId,
+          saveRasheedQuestIPFS,
+          { from: questHero, value: this.questCost }
+        ]
+        await this.questToken.submitProofs(...params).should.be.fulfilled
+        await this.questToken.approveReclaiming(saveRasheedQuestId, true, {
+          from: questCreator2
+        }).should.not.be.fulfilled
+
+        await this.questToken.reclaimLostProofs(saveRasheedQuestId, questHero, {
+          from: contractOwner,
+          gasPrice: 0
+        }).should.not.be.fulfilled
+
+        await this.questToken.approveReclaiming(saveRasheedQuestId, true, {
+          from: questCreator
+        }).should.be.fulfilled
+
+        const balance = await web3.eth.getBalance(contractOwner)
+        await this.questToken.reclaimLostProofs(saveRasheedQuestId, questHero, {
+          from: contractOwner,
+          gasPrice: 0
+        }).should.be.fulfilled
+        const balance1 = await web3.eth.getBalance(contractOwner)
+        balance.should.be.bignumber.equal(balance1.minus(this.questCost))
+
+        await this.questToken.approveReclaiming(saveRasheedQuestId, false, {
+          from: questCreator
+        }).should.be.fulfilled
+        await this.questToken.reclaimLostProofs(saveRasheedQuestId).should.not
+          .be.fulfilled
       })
     })
   }
